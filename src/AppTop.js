6f7b8e0ede4db38b05c73f582783aabcf4323798
yoko <hydra666kyo@gmail.com>
2021-01-29 02:11:37 +0900

    test

--- src/AppTop.js
+++ src/AppTop.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Header, Footer } from "./components";
-// import Header from "./components/Header";
-// import Footer from "./components/Footer";
 // import React,{useContext,useState,useReducer} from 'react';
 
 const AppTop = () => {
